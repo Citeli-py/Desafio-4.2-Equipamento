1a4a70ade0acee7f02aac6245cb8229a10a9123c
Citeli-py <matheus.citeli2001@gmail.com>
2024-12-27 13:50:14 -0300

    :sparkles: feat: implementação da classe bicicleta

--- models/Bicicleta.js
+++ models/Bicicleta.js
@@ -1,44 +1,42 @@
-const { Model, DataTypes } = require('sequelize');
+import { Model, DataTypes } from 'sequelize';
 
-module.exports = (sequelize) => {
-  class Bicicleta extends Model {}
+export class Bicicleta extends Model {
 
-  Bicicleta.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
+  static init(sequelize){
+    super.init({
+        id: {
+          type: DataTypes.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        marca: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        modelo: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        ano: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+        numero: {
+          type: DataTypes.STRING,
+          unique: true,
+          allowNull: false,
+        },
+        status: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          defaultValue: 'disponível', // Exemplos: 'disponível', 'alugada', 'manutenção'
+        },
       },
-      marca: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      modelo: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      ano: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      numero: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false,
-      },
-      status: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        defaultValue: 'disponível', // Exemplos: 'disponível', 'alugada', 'manutenção'
-      },
-    },
-    {
-      sequelize,
-      modelName: 'Bicicleta',
-      tableName: 'bicicletas',
-    }
-  );
-
-  return Bicicleta;
+      {
+        sequelize,
+        modelName: 'Bicicleta',
+        tableName: 'bicicletas',
+      }
+    );  
+  }
 };
