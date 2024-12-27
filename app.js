0188bf8f909c50fa7732748574138d1f7a935d66
Citeli-py <matheus.citeli2001@gmail.com>
2024-12-27 14:56:52 -0300

    Adaptação para ES6 e Database

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const sequelize = require('./config/database');
-const taskRoutes = require('./routes/taskRoutes');
-const luxon = require('luxon');
+import express from 'express';
+import bodyParser from 'body-parser';
+import bicicletaRoutes from './routes/bicicletaRoutes.js';
+import {DateTime} from 'luxon';
 
 const app = express();
 const PORT = 3000;
@@ -12,16 +11,21 @@ app.use(bodyParser.json());
 
 // Middleware logs
 app.use(function (req, res, next) {
-  const agora = new luxon.DateTime(Date.now())
+  const agora = new DateTime(Date.now())
   console.log(agora.toFormat("dd/MM/yyyy - HH:mm:ss"), req.method, req.url,);
   next();
 });
 
 // Rotas
-app.use('/api/tasks', taskRoutes);
+app.use('/bicicleta', bicicletaRoutes);
+
+import Database from './db/Database.js';
+
+Database.init();
+const res = await Database.autenticacao()
 
 // Sincronizar banco de dados e iniciar o servidor
-sequelize.sync({ force: false }) // Use { force: true } apenas para recriar tabelas ao desenvolver
+Database.conexao.sync({ force: true }) // Use { force: true } apenas para recriar tabelas ao desenvolver
   .then(() => {
     console.log('Database synced successfully.');
     app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
