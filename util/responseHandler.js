a938c5a122f8155646182d9ff9c2c5a023bd9beb
Citeli-py <matheus.citeli2001@gmail.com>
2025-01-04 11:46:25 -0300

    BUG: Correção na escrita do log

--- util/responseHandler.js
+++ util/responseHandler.js
@@ -34,30 +34,32 @@ class ErroInterno{
      * @param {Error} error - Objeto de erro capturado.
      * @param {string} context - Contexto ou descrição do local onde o erro ocorreu.
      */
-    logErrorToFile(error, context = '') {
+    static logErrorToFile(error, context = '') {
 
         // Caminho para o arquivo de log
         const __dirname = path.resolve(); // Para obter o diretório atual
-        const logFilePath = path.join(__dirname, '../log/errors.log');
+        const logFilePath = path.join(__dirname, '/log/erros.log');
 
         const timestamp = new Date().toISOString(); // Data e hora formatada
-        const logMessage = `
-            [${timestamp}] - ERRO:
+        const logMessage = `[${timestamp}] - ERRO:
             Contexto: ${context}
             Mensagem: ${error.message}
             Stack Trace: ${error.stack || 'N/A'}
-            --------------------------------------------
-        `;
+            --------------------------------------------\n`;
 
         // Escreve no arquivo de log (cria se não existir)
-        fs.appendFile(logFilePath, logMessage);
+        fs.appendFile(logFilePath, logMessage, (err) => {
+            if (err) {
+                console.error('Erro ao escrever no arquivo de log:', err.message);
+            }
+        });
     }
 
 
-    static toResponse(res, codigo, mensagem){
+    static toResponse(res, codigo, erro, contexto){
         // Escrever no log
-        this.logErrorToFile(mensagem);
-        return ResponseHandler.error(res, 500, mensagem, codigo);
+        this.logErrorToFile(erro, contexto);
+        return ResponseHandler.error(res, 500, "Erro no servidor", codigo);
     }
 }
 
