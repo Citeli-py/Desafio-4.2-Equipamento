802c76f1969ada61a31e8fa43380a891d4f68766
Citeli-py <matheus.citeli2001@gmail.com>
2025-01-06 15:46:26 -0300

    Retirada de metodos dos modelos, transferidos para os Repos

--- models/Tranca.js
+++ models/Tranca.js
@@ -49,44 +49,6 @@ export class Tranca extends Model {
     );
   }
 
-  /**
-   * Esse metodo tranca a Tranca, alterando seu status para OCUPADA e associando uma bicicleta a ela, 
-   * essa bicicleta tem seu status trocado para disponível
-   * @param {Bicicleta} bicicleta  - Bicicleta que sera presa na tranca
-   */
-  async trancar(bicicleta=null){
-    
-    if (bicicleta){
-      this.bicicleta = bicicleta.id;
-      bicicleta.status = "DISPONIVEL";
-      await bicicleta.save();
-    }
-    
-    this.status = "OCUPADA";
-    await this.save();
-  }
-
-  /**
-   * Metodo para destrancar a tranca, retirando a associação com uma bicicleta e alterando seu status para LIVRE
-   */
-  async destrancar(){
-    this.bicicleta = null;
-    this.status = "LIVRE";
-    await this.save()
-  }
-
-  /**
-   * 
-   * @param {Bicicleta} bicicleta - Bicicleta que pode estar na tranca
-   * @returns {boolean} - Se existir uma bicicleta na tranca retorna true
-   */
-  isBicicletaNaTranca(bicicleta=null){
-    if(bicicleta)
-      return this.bicicleta === bicicleta.id;
-
-    return this.bicicleta !== null;
-  }
-  
   static associate(models) {
     this.belongsTo(models.Bicicleta, { foreignKey: 'bicicleta', as: 'bicicleta' });
   }
