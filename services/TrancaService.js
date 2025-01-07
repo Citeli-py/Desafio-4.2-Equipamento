d4b9a6405e92cd0287ee7f332b5e49003e9344aa
Citeli-py <matheus.citeli2001@gmail.com>
2025-01-07 09:02:50 -0300

    Métodos para implementar obter tranca

--- services/TrancaService.js
+++ services/TrancaService.js
@@ -8,4 +8,23 @@ export class TrancaService {
         return await TrancaRepo.getAllTrancas();
     }
 
+    static async obterTranca(id){
+        const tranca = await TrancaRepo.getTranca(id);
+        if (!tranca)
+            return {sucesso: false, erro: DadoNaoEncontrado, mensagem: "Tranca não encontrada"};
+
+        return {
+            sucesso: true, 
+            tranca: {
+                id:tranca.id,
+                bicicleta:tranca.bicicleta,
+                numero:tranca.numero,
+                localizacao:tranca.localizacao,
+                anoDeFabricacao:tranca.anoDeFabricacao,
+                modelo:tranca.modelo,
+                status:tranca.status
+            }
+        };
+    }
+
 }
