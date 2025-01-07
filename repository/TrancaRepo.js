8014babc42de303dd495c10704b68d514c35baf1
Citeli-py <matheus.citeli2001@gmail.com>
2025-01-07 14:56:34 -0300

    Deletar tranca e refatoração para lidar com commits e rollbacks

--- repository/TrancaRepo.js
+++ repository/TrancaRepo.js
@@ -1,6 +1,8 @@
-import { Op, ValidationError } from 'sequelize';
+import { Op, Sequelize, Transaction, ValidationError } from 'sequelize';
 import { Tranca } from "../models/Tranca.js"  
 import { DadoInvalido } from '../util/erros.js';
+
+import Database from '../db/Database.js';
 
 export class TrancaRepo {
 
@@ -38,16 +40,18 @@ export class TrancaRepo {
      * @param {string} anoDeFabricacao 
      * @param {string} modelo 
      * @param {string} status 
+     * @param {null|Transaction} [transacao_externa=null] 
      * @returns {{sucesso: boolean, tranca?: Tranca, erro?: number, mensagem?: string}}
      * 
      * @throws {ValidationError}
      */
-    static async criarTranca(numero, localizacao, anoDeFabricacao, modelo, status){
+    static async criarTranca(numero, localizacao, anoDeFabricacao, modelo, status, transacao_externa=null){
         try {
             // O status inicial da tranca será “nova” (esta informação não pode ser editada).
-            const tranca = await Tranca.create({numero, localizacao, anoDeFabricacao, modelo, status: "NOVO"})
+            const tranca = await Tranca.create({numero, localizacao, anoDeFabricacao, modelo, status: "NOVO"}, {transaction: transacao_externa})
             return {sucesso: true, tranca }
         } catch(error) {
+            
             if (error instanceof ValidationError) 
                 return { sucesso: false, erro: DadoInvalido, mensagem: 'Dados inválidos' };
 
@@ -55,14 +59,37 @@ export class TrancaRepo {
         }
     }
 
-    static async editarTranca(tranca, dados){
+    /**
+     * 
+     * @param {Tranca} tranca 
+     * @param {Object} dados 
+     * @param {Transaction|null} [transacao_externa=null] 
+     * @returns {{sucesso: boolean, erro?: number, mensagem?: string, tranca?: Tranca}}
+     */
+    static async editarTranca(tranca, dados, transacao_externa=null){
         try{
-            const trancaEditada = await tranca.update(dados);
+            const trancaEditada = await tranca.update(dados, {transaction: transacao_externa});
             return {sucesso: true, tranca: trancaEditada};
+
         } catch (error){
             if (error instanceof ValidationError) 
                 return { sucesso: false, erro: DadoInvalido, mensagem: 'Dados inválidos' };
 
+            throw error;
+        }
+    }
+
+    /**
+     * realiza um softdelete na tranca
+     * @param {Tranca} tranca - Tranca
+     * @param {null|Transaction} [transacao_externa=null] 
+     */
+    static async deletarTranca(tranca, transacao_externa=null){
+        try{
+            await tranca.update({ status: 'EXCLUIDA' }, {transaction: transacao_externa});
+            return {sucesso: true, tranca: tranca};
+
+        } catch(error){
             throw error;
         }
     }
@@ -73,26 +100,47 @@ export class TrancaRepo {
      * 
      * @param {Tranca} tranca - Tranca que será trancada
      * @param {Bicicleta} bicicleta  - Bicicleta que sera presa na tranca
+     * @param {Transaction|null} [transacao_externa=null] 
      */
-    static async trancar(tranca, bicicleta=null){
-    
-        if (bicicleta){
-            tranca.bicicleta = bicicleta.id;
-            bicicleta.status = "DISPONIVEL";
-            await bicicleta.save();
+    static async trancar(tranca, bicicleta=null, transacao_externa=null){
+        let transaction = transacao_externa
+        if(!transacao_externa)
+            transaction = await Database.createTransaction();
+
+        try{
+            if (bicicleta){
+                tranca.bicicleta = bicicleta.id;
+                bicicleta.status = "DISPONIVEL";
+                await bicicleta.save({transaction});
+            }
+            
+            tranca.status = "OCUPADA";
+            await tranca.save({transaction});
+
+            // Caso não haja uma transação acima dessa
+            if(!transacao_externa)
+                transaction.commit();
+
+        } catch(error){
+            await transaction.rollback();
+            throw error;
         }
-        
-        tranca.status = "OCUPADA";
-        await tranca.save();
     }
 
     /**
      * Metodo para destrancar a tranca, retirando a associação com uma bicicleta e alterando seu status para LIVRE
+     * @param {Tranca} tranca 
+     * @param {Transaction|null} [transacao_externa=null] - Transação a ser associada
      */
-    static async destrancar(tranca){
-        tranca.bicicleta = null;
-        tranca.status = "LIVRE";
-        await tranca.save()
+    static async destrancar(tranca, transacao_externa=null){
+        try{
+            tranca.bicicleta = null;
+            tranca.status = "LIVRE";
+            await tranca.save({transaction: transacao_externa});
+
+        } catch(error){
+            throw error;
+        }
     }
 
     /**
