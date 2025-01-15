67f6de63d570bc555ca8483a6cad715ee09031cc
Citeli-py <matheus.citeli2001@gmail.com>
2025-01-15 09:22:05 -0300

    Tratamento de erro 'ERR_BAD_REQUEST'

--- src/api/aluguel.js
+++ src/api/aluguel.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export class AluguelApi {
-    static url = "http://20.226.53.175:8001/turma-node-grupo-4-aluguel/funcionario";
-    //static url = "http://localhost:3000/funcionario";
+    //static url = "http://20.226.53.175:8001/turma-node-grupo-4-aluguel/funcionario";
+    static url = "http://localhost:3000/funcionario";
 
     /**
      * Busca informações de um funcionário por ID.
@@ -11,12 +11,13 @@ export class AluguelApi {
      */
     static async getFuncionario(id) {
         try {
-            const response = await axios.get(`${this.url}/${id}`);
+            const response = await axios.get(`${this.url}/${id}`, { timeout: 5000 });
             return response.data; // Retorna os dados do funcionário.
 
         } catch (error) {
+
             // Não encontrou funcionário
-            if (error.response) 
+            if (error.code === "ERR_BAD_REQUEST") 
                 return null
 
             // Retorna um erro genérico em caso de falha na requisição.
